b8738d1dd7b127833dfd4ecefde1862240266536
MediumThor <liltimefrog@protonmail.com>
2023-08-26 11:09:49 -0500

    disable membership form

--- src/components/Dialog/Membership.js
+++ src/components/Dialog/Membership.js
@@ -11,7 +11,10 @@ import firebase from 'firebase/app';
 import { firestore as db } from '../../../firebase';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import { Typography } from '@material-ui/core';
 
 
 const StyledText = styled.p`
@@ -54,7 +57,7 @@ const MembershipDialog = ({ open, onClose, scroll }) => {
         length: '',
         occupation: '',
         interests: '',
-        // ... other fields
+        ownBoat: 'Yes', // Default value
     });
 
     const handleChange = (e) => {
@@ -62,18 +65,28 @@ const MembershipDialog = ({ open, onClose, scroll }) => {
         setForm((prev) => ({ ...prev, [name]: value }));
     };
 
+
     const handleSubmit = () => {
         db.collection('newApplications').add(form)
             .then((docRef) => {
                 console.log('Document written with ID: ', docRef.id);
-                setUploadSnackbarOpen(true); // Open the Snackbar
-                setUploadSnackbarMessage('Application submitted successfully!'); // Set the message
+                setUploadSnackbarOpen(true);
+                setUploadSnackbarMessage('Application submitted successfully!');
+                // Trigger the Cloud Function
+                const sendNewApplicationEmail = functions.httpsCallable('sendNewApplicationEmail');
+                sendNewApplicationEmail()
+                    .then(() => {
+                        console.log('New application email sent');
+                    })
+                    .catch((error) => {
+                        console.error('Error sending new application email:', error);
+                    });
                 handleClose();
             })
             .catch((error) => {
                 console.error('Error adding document: ', error);
-                setUploadSnackbarOpen(true); // Open the Snackbar
-                setUploadSnackbarMessage('An error occurred. Please try again.'); // Set the error message
+                setUploadSnackbarOpen(true);
+                setUploadSnackbarMessage('An error occurred. Please try again.');
             });
     };
 
@@ -118,18 +131,83 @@ const MembershipDialog = ({ open, onClose, scroll }) => {
 
 
 
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            {/**   <Button variant="outlined" color="primary" onClick={handleClickOpen}>
                 Open Membership Form
-            </Button>
+            </Button>*/}
+
+
             <Dialog open={openForm} onClose={handleClose} aria-labelledby="form-dialog-title" fullWidth maxWidth="md">
                 <DialogTitle id="form-dialog-title">Membership Application</DialogTitle>
                 <DialogContent>
                     {/* Applicant Information */}
-                    <TextField onChange={handleChange} name="applicantName" margin="dense" label="Applicant Name" fullWidth />
-                    <TextField onChange={handleChange} name="address" margin="dense" label="Address" fullWidth />
-                    <TextField onChange={handleChange} name="city" margin="dense" label="City" fullWidth />
+                    <TextField onChange={handleChange} name="applicantName" margin="dense" label="Name(s) of Applicant" fullWidth />
+                    <TextField onChange={handleChange} name="city" margin="dense" label="Home City" fullWidth />
                     <TextField onChange={handleChange} name="state" margin="dense" label="State" fullWidth />
                     <TextField onChange={handleChange} name="zip" margin="dense" label="Zip" fullWidth />
+                    <TextField onChange={handleChange} name="occupation" margin="dense" label="Occupation(s)" fullWidth />
+                    <TextField onChange={handleChange} name="employers" margin="dense" label="Employer(s)" fullWidth />
+                    <TextField onChange={handleChange} name="interests" margin="dense" label="Hobbies or Special Interests (including those that maybe valuable to the Club)" fullWidth />
+
+                    <Typography variant="body1" color="textPrimary" gutterBottom>
+                        Do you own a boat?
+                    </Typography>
+                    <RadioGroup
+                        name="ownBoat"
+                        value={form.ownBoat}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel
+                            value="Yes"
+                            control={<Radio />}
+                            label="Yes"
+                        />
+                        <FormControlLabel
+                            value="No"
+                            control={<Radio />}
+                            label="No"
+                        />
+                        <FormControlLabel
+                            value="Intend to in the near future"
+                            control={<Radio />}
+                            label="Intend to in the near future"
+                        />
+                    </RadioGroup>
+                    <Typography variant="body1" color="textPrimary" gutterBottom>
+                        If you do not own a boat, do you regularly crew on a boat?
+                    </Typography>
+
+                    <RadioGroup
+                        name="crewBoat"
+                        value={form.crewBoat}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel
+                            value="Yes"
+                            control={<Radio />}
+                            label="Yes"
+                        />
+                        <FormControlLabel
+                            value="No"
+                            control={<Radio />}
+                            label="No"
+                        />
+                        <FormControlLabel
+                            value="I would be interested in doing so"
+                            control={<Radio />}
+                            label="I would be interested in doing so"
+                        />
+                    </RadioGroup>
+
+                    <Typography variant="body1" color="textPrimary" gutterBottom>
+                        CLUB SPONSORS:
+                    </Typography>
+                    <TextField onChange={handleChange} name="sponsor1" margin="dense" label=" Print Name" fullWidth />
+                    <TextField onChange={handleChange} name="sponsor1length" margin="dense" label="How long have you known the sponsor" fullWidth />
+                    <TextField onChange={handleChange} name="sponsor1how" margin="dense" label="Nature of acquaintance?" fullWidth />
+
+
+
+                    <TextField onChange={handleChange} name="address" margin="dense" label="Address" fullWidth />
                     <TextField onChange={handleChange} name="phone" margin="dense" label="Phone" fullWidth />
                     <TextField onChange={handleChange} name="email" margin="dense" label="Email" fullWidth />
 
@@ -145,8 +223,6 @@ const MembershipDialog = ({ open, onClose, scroll }) => {
                     {/* Additional fields for boat information */}
 
                     {/* Additional Information */}
-                    <TextField onChange={handleChange} name="occupation" margin="dense" label="Occupation" fullWidth />
-                    <TextField onChange={handleChange} name="interests" margin="dense" label="Interests" fullWidth />
 
                 </DialogContent>
                 <DialogActions>
